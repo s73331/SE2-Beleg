8550a7cb2521d1d16333de8b2dc9c34dd0ffe74a
Oliver Eberhardt <oliver@Olivers-MacBook-Pro.local>
2016-06-19 23:27:17 +0200

    Added ColorSensor, Added wait commands

---
+++ ev3steuerung/rezeptabarbeitung/ColorSensor.java
@@ -0,0 +1,72 @@
+package Maschinensteuerung;
+
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorMode;
+import lejos.robotics.Color;
+
+public class ColorSensor extends Device{
+	
+	private String port;
+	private EV3ColorSensor sensor;
+	
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
+	
+	public ColorSensor(String port){
+		this.port = port;
+		
+	}
+	
+	@Override
+	public boolean register(){
+		
+		Brick brick = BrickFinder.getDefault();
+		Port p = brick.getPort(port);
+		sensor = new EV3ColorSensor(p);
+
+		return true;
+	}
+	
+	
+	
+	@Override
+	public boolean detectColor(){
+		
+		SensorMode color = sensor.getColorIDMode();
+		float[] sample = new float[color.sampleSize()];
+		color.fetchSample(sample, 0);
+		int colorId = (int)sample[0];
+		String colorName = "";
+		switch(colorId){
+			case Color.NONE: colorName = "NONE"; break;
+			case Color.BLACK: colorName = "BLACK"; break;
+			case Color.BLUE: colorName = "BLUE"; break;
+			case Color.GREEN: colorName = "GREEN"; break;
+			case Color.YELLOW: colorName = "YELLOW"; break;
+			case Color.RED: colorName = "RED"; break;
+			case Color.WHITE: colorName = "WHITE"; break;
+			case Color.BROWN: colorName = "BROWN"; break;
+		}
+		LCD.drawString(colorId + " - " + colorName, 1, 3);
+
+		
+		return true;
+		
+	}
+	
+	public boolean close(){
+		return true;
+	}
+	
+	
+	
+
+}
---
+++ ev3steuerung/rezeptabarbeitung/Wait.java
@@ -0,0 +1,50 @@
+package Maschinensteuerung;
+
+import lejos.utility.Delay;
+
+public class Wait {
+	
+	private long ms;
+	private int mode;	// 0 = auf bestimmte Zeit warten, 1 = auf TouchSensor warten (drücken)
+						// 2 = auf TouchSensor warten (los lassen)
+	private int sensor;
+
+	public long getMs() {
+		return ms;
+	}
+	public int getMode() {
+		return mode;
+	}
+
+	public void setMode(int mode) {
+		this.mode = mode;
+	}
+	public void setMs(long ms) {
+		this.ms = ms;
+	}
+	public int getSensor() {
+		return sensor;
+	}
+	public void setSensor(int sensor) {
+		this.sensor = sensor;
+	}
+	
+	public Wait(long ms, int mode){
+		this.ms = ms;
+		this.mode = mode;
+	}
+	public Wait(int sensor, int mode){
+		this.sensor = sensor;
+		this.mode = mode;
+	}
+	
+	
+	
+	public boolean waitTime() {
+		Delay.msDelay(ms);
+		return true;
+	}
+	
+
+	
+}
