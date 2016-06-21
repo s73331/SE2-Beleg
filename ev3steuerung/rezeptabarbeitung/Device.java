13095075e4cd001b72bb667699330a121cf70765
Oliver Eberhardt <oliver@Olivers-MacBook-Pro.local>
2016-06-21 17:27:28 +0200

    comments for Device.java

--- ev3steuerung/rezeptabarbeitung/Device.java
+++ ev3steuerung/rezeptabarbeitung/Device.java
@@ -4,46 +4,86 @@ import lejos.utility.Delay;
 
 public abstract class Device {
 	
+    
+    /**
+     *rotates the motor with the specified speed and angle from the recipe.
+     *@return true
+     */
 	public boolean rotate(boolean mode,int speed, int angle){
 		return true;
 	}
 	
+    /**
+     *Close the device on the specified port.
+     *@return true
+     */
 	public boolean close(){
 		return true;
 	}
+    
+    /**
+     *Registers the device with the specified port.
+     *@return true
+     */
 	public boolean register(){
 		return true;
 	}
 	
+    /**
+     *Checks if the TouchSensor is pressed
+     *@return true
+     */
 	public boolean isPressed(){
 		return true;
 	}
 	
+    /**
+     *Waits until the TouchSensor is pressed.
+     *@return true
+     */
 	public boolean waitForPress(){
 		return true;
 	}
 	
+    
+    /**
+     *Waits until the TouchSensor is released.
+     *@return true
+     */
 	public boolean waitForRelease(){
 		return true;
 	}
 
+    /**
+     *rotates the motor with the specified speed from the recipe
+     *no defined angle
+     *@return true
+     */
 	public boolean forward(int speed) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+    /**
+     *stops the device
+     *@return false
+     */
 	public boolean stop() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+    /**
+     *Detect the color on the ColorSensor and display the detected color on the LCD of the EV3.
+     *@return false
+     */
 	public boolean detectColor() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+    /**
+     *Checks if the TouchSensor is not pressed
+     *@return false
+     */
 	public boolean isNotPressed() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
