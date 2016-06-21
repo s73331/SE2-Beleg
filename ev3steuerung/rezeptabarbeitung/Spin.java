0c062b8a180ca3866fa2070b53b59b733eb85836
Oliver Eberhardt <oliver@Olivers-MacBook-Pro.local>
2016-06-21 16:50:13 +0200

    comments for Spin.java

--- ev3steuerung/rezeptabarbeitung/Spin.java
+++ ev3steuerung/rezeptabarbeitung/Spin.java
@@ -9,48 +9,92 @@ public class Spin {
 	private int sensor;
 	private int till;
 
+    /**
+     *@return angle
+     */
 	public int getAngle() {
 		return angle;
 	}
 
+    /**
+     *@param angle for how far the motors spins
+     */
 	public void setAngle(int angle) {
 		this.angle = angle;
 	}
 
+    /**
+     *@return speed
+     */
 	public int getSpeed() {
 		return speed;
 	}
 
+    /**
+     *@param speed for the motor in angle per second
+     */
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
 	
+    /**
+     *@return device
+     */
 	public int getDevice() {
 		return device;
 	}
 
+    /**
+     *@param device. Which motor should spin
+     */
 	public void setDevice(int device) {
 		this.device = device;
 	}
 
+    /**
+     *The mode for the motor and sensor coorporation.
+     *0 = spin till TouchSensor is released
+     *1 = Spin till TouchSensor is pressed
+     *9 = Spin the specified angle
+     *@return till
+     */
 	public int getTill() {
 		return till;
 	}
 
+    /**
+     *The mode for the motor and sensor coorporation.
+     *0 = spin till TouchSensor is released
+     *1 = Spin till TouchSensor is pressed
+     *9 = Spin the specified angle
+     *@param till
+     */
 	public void setTill(int till) {
 		this.till = till;
 	}
 	
+    /**
+     *@return sensor
+     */
 	public int getSensor() {
 		return sensor;
 	}
 
+    /**
+     *@param sensor on which the motor should react for press/release
+     */
 	public void setSensor(int sensor) {
 		this.sensor = sensor;
 	}
 
-	
-	/*Drehen bis Touchsensor gedrückt(till = 1), losgelassen(till = 0) oder bis Gradzahl(till = 9)*/
+	/**
+     *Constructor
+     *@param speed
+     *@param angle
+     *@param device
+     *@param sensor
+     *@param till
+     */
 	public Spin(int angle, int speed, int device, int sensor, int till){
 		this.speed = speed;
 		this.angle = angle;
@@ -58,10 +102,6 @@ public class Spin {
 		this.setSensor(sensor);
 		this.setTill(till);
 	}
-	
-
-
-
 
 	
 }
